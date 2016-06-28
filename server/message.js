cdc9a3d9e885bcc5ca9196818f5a2338f4abfe50
mredmundto <yatlunto@gmail.com>
2016-06-27 20:28:48 -0700

    Add Message class

---
+++ server/message.js
@@ -0,0 +1,15 @@
+class Message {
+  constructor(username, text) {
+    var date = new Date();
+    var dateStr = date.toISOString;
+
+    this.createdAt = dateStr;
+    this.objectId = date.valueOf();
+    this.roomname = 'lobby';
+    this.text = text;
+    this.updatedAt = dateStr;
+    this.username = username;
+  }
+}
+
+module.exports = Message;
